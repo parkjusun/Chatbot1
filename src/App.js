ff4597f54e114375815cd58982a8891152a05f7d
박주선 <62981020+parkjusun@users.noreply.github.com>
2020-07-07 12:34:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Home,ChatBot } from './components';
+import { Home,MyChatBot } from './components';
 
 function App() {
   return (
       <Router>
         <Link to="/">홈으로 이동</Link> <br/>
         <Link to="/chat">챗봇 이동</Link>
-        <Route exact path='/chat' component={ChatBot}/>
+        <Route exact path='/chat' component={MyChatBot}/>
         <Route exact path='/' component={Home}/>
       </Router>
   );
